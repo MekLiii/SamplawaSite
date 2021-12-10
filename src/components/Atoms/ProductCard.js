fd8c3aa1a482d141bcbc1526902b7bb3c61f7877
kamil_desktop <theczacharek@gmail.com>
2021-12-10 17:04:47 +0100

    as

---
+++ src/components/Atoms/ProductCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+function ProductCard({name,price,img}) {
+  return (
+    <Card>
+      <Box>
+        <Img src={img}/>
+        <P>Szalik</P>
+        <P>30zł</P>
+      </Box>
+    </Card>
+  );
+}
+const Card = styled.div`
+  width: 400px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #1d1d1d;
+  border:15px solid #2b2b2b;
+
+`;
+const Box = styled.div`
+  width: 90%;
+  height: 90%;
+  background-color:#1d1d1d;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Img = styled.img`
+    width: 100%;
+    height: 50%;
+`
+const P = styled.p`
+
+`
+export default ProductCard;
---
+++ src/pages/Sklep.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import ProductCard from "../components/Atoms/ProductCard";
+import Layout from "../components/Organism/Layout";
+
+function Sklep() {
+  return (
+    <Layout>
+      <Cointainer>
+        <GridHolder>
+            <ProductCard />
+        </GridHolder>
+
+      </Cointainer>
+    </Layout>
+  );
+}
+const Cointainer = styled.div`
+  height: 82vh;
+`;
+const GridHolder = styled.div`
+
+`
+
+export default Sklep;
