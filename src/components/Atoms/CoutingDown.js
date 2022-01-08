50309e1ec815ecd3dcf3a4e737ec96f6d1d805c5
kamil_desktop <theczacharek@gmail.com>
2022-01-08 14:11:07 +0100

    8

--- src/components/Atoms/CoutingDown.js
+++ src/components/Atoms/CoutingDown.js
@@ -1,37 +1,38 @@
 import React from "react";
 import DateCountdown from "react-date-countdown-timer";
 import styled from "styled-components";
-import './app.css'
+import "./app.css";
+
 
 function CoutingDown() {
   if (typeof window !== `undefined`) {
     return <div></div>
   }
-    
+
   return (
     <Box>
-        <P>Na boisku widzimy się za:</P>
+      <P>Na boisku widzimy się za:</P>
+      
       <DateCountdown dateTo="04/12/2022" callback={() => alert("Hello")} style={{fontSize:'clamp(12px,2vw,30px)',color:'white',fontFamily:'poppins'}} />
     </Box>
   );
 }
+
 const Box = styled.div`
   width: 100%;
   height: 10vh;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: space-around;
-  @media (max-width: 800px){
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
+  @media (max-width: 800px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
-  
- 
 `;
 const P = styled.p`
-    font-size: clamp(20px, 3vw, 50px);
-    color:white;
-    font-family: poppins;
-`
+  font-size: clamp(20px, 3vw, 50px);
+  color: white;
+  font-family: poppins;
+`;
 export default CoutingDown;
