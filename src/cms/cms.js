4938e0402530f69fac5c598983d8248fb49437bf
Kamil Czacharowski <72619640+MekLiii@users.noreply.github.com>
2023-07-27 14:10:34 +0200

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,7 +4,7 @@ import uploadcare from 'netlify-cms-media-library-uploadcare';
 CMS.registerMediaLibrary(uploadcare);
 document.addEventListener("DOMContentLoaded", () => {
     uploadcare.Widget('[name="file"]', {
-        "publicKey": "075151893a65319cd6da",
+        "publicKey": "",
         "tabs": "file camera url facebook gdrive gphotos"
     })
 });
